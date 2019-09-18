81c402182e5eff1edde4f6478ef8ec27169e6977
dybbukk <45940717+dybbukk@users.noreply.github.com>
2019-09-18 01:12:04 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This notebook will be mainly used for the capstone project
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course')
+
+
+# In[ ]:
+
+
+
+
